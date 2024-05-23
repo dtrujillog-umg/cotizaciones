a96d900bce77e1b790d2091830116c647f38371f
Diego Trujillo <diegojav_89@hotmail.com>
2024-05-23 14:24:01 -0600

    2023052024

--- Backend/Vehiculos.mjs
+++ Backend/Vehiculos.mjs
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     cb(null, "../Frontend/Images/nuevos/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + '-' + file.originalname);ra
   },
 });
 
